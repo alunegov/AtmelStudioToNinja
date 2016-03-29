4bc9ec3e7c99840025d15ee52156808b745b4b44
Alexander Lunegov <alunegov@gmail.com>
2016-03-29 14:00:44 +0500

    Use argparse

--- asninja.py
+++ asninja.py
@@ -40,9 +40,9 @@ class AtmelStudioToolchain(Toolchain):
             return AtmelStudio62Toolchain(prj_version, name, flavour)
         else:
             if prj_version == '7.0':
-                return AtmelStudio70Toolchain(prj_version, name, flavour)
-            else:
-                assert False, 'Unsupported project version'
+            return AtmelStudio70Toolchain(prj_version, name, flavour)
+        else:
+            assert False, 'Unsupported project version'
 
     def path(self):
         if self.flavour == 'Native':
@@ -505,10 +505,10 @@ def convert(as_prj, config, outpath, output, flags, add_defs, del_defs):
             obj_files += nw.build('$builddir/' + filename + '.o', 'cc', '$src/' + src_file)
         else:
             if file_ext == '.cpp':
-                assert asp.is_cpp
-                obj_files += nw.build('$builddir/' + filename + '.o', 'cxx', '$src/' + src_file)
-                # else:
-                # print('Skipping file {}'.format(src_file))
+            assert asp.is_cpp
+            obj_files += nw.build('$builddir/' + filename + '.o', 'cxx', '$src/' + src_file)
+        # else:
+        #     print('Skipping file {}'.format(src_file))
 
     if obj_files:
         nw.newline()
@@ -537,30 +537,28 @@ def convert(as_prj, config, outpath, output, flags, add_defs, del_defs):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        _as_prj = sys.argv[1]
-        _config = sys.argv[2]
-        _outpath = sys.argv[3]
-        _output = sys.argv[4]
-        _flags = sys.argv[5].split(' ')
-        if len(sys.argv) > 6:
-            _add_defs = sys.argv[6].split(' ')
-        else:
-            _add_defs = []
-        _del_defs = []
-    else:
-        _as_prj = os.path.join('tests', 'Korsar3.cproj')
-        _config = 'Debug'
-        _outpath = _config
-        _output = 'Korsar3'
-        _flags = ['-mthumb', '-mcpu=cortex-m4']
-        _add_defs = ['__SAM4S8C__']
-        _del_defs = []
+    import argparse
 
-    convert(as_prj=_as_prj,
-            config=_config,
-            outpath=_outpath,
-            output=_output,
-            flags=_flags,
-            add_defs=_add_defs,
-            del_defs=_del_defs)
+    parser = argparse.ArgumentParser(description='asninja')
+    parser.add_argument('--prj', type=str, help='Atmel Studio project file')
+    parser.add_argument('--config', type=str, help='Configuration (Debug, Release, ...)', default='Debug')
+    parser.add_argument('--outpath', type=str, help='Output path (if absent when same as configuration name)',
+                        default=None)
+    parser.add_argument('--output', type=str, help='Output filename')
+    parser.add_argument('--flags', type=str, help='Additional compiler and linker flags (like -mthumb)', default=None)
+    parser.add_argument('--add_defs', type=str, help='Additional compiler defines (like __SAM4S8C__)', default=None)
+    parser.add_argument('--del_defs', type=str, help='Additional compiler undefines', default=None)
+    # parser.add_argument('--toolchain', type=str, help='GCC toolchain path', default=None)
+
+    # get all data from command line
+    args = parser.parse_args()
+    # print(args)
+
+    _outpath = args.outpath if args.outpath else args.config
+    _flags = args.flags.split(' ') if args.flags else []
+    _add_defs = args.add_defs.split(' ') if args.add_defs else []
+    _del_defs = args.del_defs.split(' ') if args.del_defs else []
+    # print(_flags, _add_defs, _del_defs)
+
+    convert(as_prj=args.prj, config=args.config, outpath=_outpath, output=args.output, flags=_flags,
+            add_defs=_add_defs, del_defs=_del_defs)
